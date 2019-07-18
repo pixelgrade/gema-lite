ddf28ed0dc37a313f43dae1df8daa8663c2e6727
madalingorbanescu <madalin@pixelgrade.com>
2019-07-18 11:17:22 +0300

    fixed skip link focus to close #22

--- functions.php
+++ functions.php
@@ -137,6 +137,8 @@ function gema_lite_scripts() {
 	/* Enqueue the main theme script file */
 	wp_enqueue_script( 'gema-scripts', get_parent_theme_file_uri( 'assets/js/main.js' ), array( 'jquery', 'bricklayer', 'imagesloaded', 'hoverIntent' ), $theme->get( 'Version' ), true );
 
+	wp_enqueue_script( 'skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+
 	if ( is_singular() && comments_open() && get_option('thread_comments') ) {
         wp_enqueue_script('comment-reply');
     }
